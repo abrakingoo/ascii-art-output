a94b3984744073fde18c428656c8b905fd11a9e5
aaochieng <aaochieng>
2024-06-06 13:55:22 +0300

    update: added exit status to 0

--- utilities/error.go
+++ utilities/error.go
@@ -1,11 +1,15 @@
 package ascii
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 // ErrHandler function is used in handiling errors from the main programme
 // printing the errors accordigly.
 func ErrHandler(err error) {
 	if err != nil {
 		fmt.Println("Error: ", err)
+		os.Exit(0)
 	}
 }
