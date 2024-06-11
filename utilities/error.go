41a9870345627fb4ab1d2463b28a3c31e29be9cf
abrakingoo <maikoabraham68@gmail.com>
2024-06-11 12:10:59 +0300

    Fix: Error handling

--- utilities/error.go
+++ utilities/error.go
@@ -1,15 +1,11 @@
 package ascii
 
-import (
-	"fmt"
-	"os"
-)
+import "fmt"
 
 // ErrHandler function is used in handiling errors from the main programme
 // printing the errors accordigly.
 func ErrHandler(err error) {
 	if err != nil {
 		fmt.Println("Error: ", err)
-		os.Exit(0)
 	}
 }
