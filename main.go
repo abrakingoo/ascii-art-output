80bfdab58b609a3d08f15ea88b1f542b7cbea33f
abrakingoo <maikoabraham68@gmail.com>
2024-06-12 12:11:01 +0300

    updated comments in main function

--- main.go
+++ main.go
@@ -11,42 +11,49 @@ import (
 	ascii "ascii/utilities"
 )
 
-func main() {
-	file_name := ""
-	banner := ""
-	ascii_art := ""
-	input := ""
-	art_switch := true
-	if len(os.Args[1:]) == 1 {
-		file_name = "bannerfiles/standard.txt"
-		input = os.Args[1]
-		art_switch = false
-	}
 
-	if len(os.Args) != 4 && art_switch{
-		fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard")
-		return
-	}
+	func main() {
+		file_name := ""     // Initialize variable for file name.
+		banner := ""        // Initialize variable for banner.
+		ascii_art := ""     // Initialize variable for ASCII art.
+		input := ""         // Initialize variable for user input.
+		art_switch := true // Initialize switch for ASCII art.
 	
-	if len(os.Args) > 2 {
-
-		input = os.Args[2] // user input
+		// Check if only one argument is provided from command line.
+		if len(os.Args[1:]) == 1 {
+			file_name = "bannerfiles/standard.txt"
+			input = os.Args[1]
+			art_switch = false
+		}
 	
-		file_name = "bannerfiles/" + os.Args[3] + ".txt"
-	}
-
-	flag.StringVar(&banner, "output", "", "")
-	flag.Parse()
-
-	if !strings.Contains(os.Args[1], "--output=") && art_switch{
-		fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard")
-		return
-	}
-	if !strings.HasSuffix(os.Args[1], ".txt") && art_switch {
-		fmt.Println("Usage: --output=<fileName.txt> only files ending with .txt are allowed")
-		return
-	}
-
+		// Check if the number of arguments is not equal to 4 and ASCII art switch is on.
+		if len(os.Args) != 4 && art_switch {
+			fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard")
+			return
+		}
+	
+		// Check if more than two arguments are provided.
+		if len(os.Args) > 2 {
+			input = os.Args[2]                                     // Set user input.
+			file_name = "bannerfiles/" + os.Args[3] + ".txt" // Set file name.
+		}
+	
+		flag.StringVar(&banner, "output", "", "") // Set banner to the value of the 'output' flag.
+		flag.Parse()                               // Parse command line flags.
+	
+		// Check if the first argument does not contain '--output=' and ASCII art switch is on.
+		if !strings.Contains(os.Args[1], "--output=") && art_switch {
+			fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard")
+			return
+		}
+	
+		// Check if the first argument does not have a suffix of '.txt' and ASCII art switch is on.
+		if !strings.HasSuffix(os.Args[1], ".txt") && art_switch {
+			fmt.Println("Usage: --output=<fileName.txt> only files ending with .txt are allowed")
+			return
+		}
+	
+	
 
 	// fmt.Println(len(input))
 
@@ -89,10 +96,9 @@ func main() {
 			for _, char := range part { // iterates through each character of the current word
 				startingIndex := ascii.GetStartingIndex(int(char)) // obtaining the starting position of the char
 				if startingIndex >= 0 {
-					if !art_switch{
-						fmt.Print(fileData[startingIndex+i]) 	
+					if !art_switch {
+						fmt.Print(fileData[startingIndex+i])
 					} else {
-
 						ascii_art += fileData[startingIndex+i] // printing the character line by line
 					}
 				}
@@ -100,7 +106,6 @@ func main() {
 			if !art_switch {
 				fmt.Println()
 			} else {
-
 				ascii_art += "\n" // printing a new line after printing each line of the charcter
 			}
 		}
