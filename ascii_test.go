53517842cd956c3e519a6aa9b804fa6fd8eca747
abrakingoo <maikoabraham68@gmail.com>
2024-06-12 12:11:23 +0300

    Tests: Test files The ascii test function test for any errors in the whole code. The tests written are also for some functions.
    TestCheckIllegalchar checks for anyillegal characters in a given string not in the ascii range of 32 - 126.
    TestHandleNewLine check whether the given string has newline escape characters ,tab,return carriage.
    TestStringContain checks whether a string has "o" characters as used in the thinkertoy.If it doest the file will also be split using carriage return.

--- ascii_test.go
+++ ascii_test.go
@@ -1,15 +1,17 @@
+// Package main provides a set of tests for ASCII utility functions.
+
 package main
 
 import (
-	"os"
-	"os/exec"
 	"reflect"
 	"testing"
 
-	ascii "ascii/utilities"
+	ascii "ascii/utilities" // Importing ASCII utilities package.
 )
 
+// TestCheckIllegalChar tests the CheckIllegalChar function.
 func TestCheckIllegalChar(t *testing.T) {
+	// Test cases for CheckIllegalChar function.
 	tests := []struct {
 		name    string
 		input   []string
@@ -35,6 +37,7 @@ func TestCheckIllegalChar(t *testing.T) {
 			wantErr: false,
 		},
 	}
+	// Running tests for CheckIllegalChar.
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := ascii.CheckIllegalChar(tt.input)
@@ -49,8 +52,9 @@ func TestCheckIllegalChar(t *testing.T) {
 	}
 }
 
+// TestHandleNewLine tests the HandleNewLine function.
 func TestHandleNewLine(t *testing.T) {
-
+	// Test cases for HandleNewLine function.
 	tests := []struct {
 		name    string
 		input   string
@@ -76,6 +80,7 @@ func TestHandleNewLine(t *testing.T) {
 			wantErr: false,
 		},
 	}
+	// Running tests for HandleNewLine.
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := ascii.HandleNewLine(tt.input)
@@ -90,7 +95,9 @@ func TestHandleNewLine(t *testing.T) {
 	}
 }
 
+// TestStringContain tests the StringContain function.
 func TestStringContain(t *testing.T) {
+	// Test cases for StringContain function.
 	tests := []struct {
 		name string
 		s    string
@@ -112,122 +119,12 @@ func TestStringContain(t *testing.T) {
 			want: []string{""},
 		},
 	}
+	// Running tests for StringContain.
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := ascii.StringContain(tt.s); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("StringContain() = %v, want %v", got, tt.want)
 			}
 		})
-	}
-}
-func TestExecCommand(t *testing.T) {
-	type Args struct {
-		str  string
-		file string
-	}
-
-	fileString := ""
-
-	// Create files
-	expected, err := os.Create("expected.txt")
-	if err != nil {
-		t.Errorf("Error :%v", err)
-	}
-	got, err := os.Create("got.txt")
-	if err != nil {
-		t.Errorf("Error :%v", err)
-	}
-	// close the files
-	defer expected.Close()
-	defer got.Close()
-
-	// Modify here : Input your test input string
-	testStrings := [4]Args{
-		{"hello", ""},
-		{"hello", "standard"},
-		{"hello", "thinkertoy"},
-		{"hello", "shadow"},
-	}
-	// Define the commands to execute here:
-	commands := [4]*exec.Cmd{
-		exec.Command("go", "run", ".", "--output=test01.txt", "hello", "standard"),
-		exec.Command("go", "run", ".", "--output=test02.txt", "hello", "thinkertoy"),
-		exec.Command("go", "run", ".", "--output=test03.txt", "hello", "shadow"),
-		exec.Command("go", "run", ".", "--output=test04.txt", "hello", "standard"),
-	}
-
-	//
-	for i, val := range testStrings {
-		// file to use
-		fileData := []string{}
-		if val.file == "" {
-			file, err := os.ReadFile("standard.txt")
-			if err != nil {
-				t.Error(err)
-				os.Exit(1)
-			}
-			fileData = append(fileData, ascii.StringContain(string(file))...)
-
-		} else {
-			filename := val.file + ".txt"
-			file, err := os.ReadFile(filename)
-			if err != nil {
-				t.Error(err)
-				os.Exit(1)
-			}
-			fileData = append(fileData, ascii.StringContain(string(file))...)
-		}
-
-		inputParts, err := ascii.HandleNewLine(testStrings[i].str)
-		if err != nil {
-			t.Error(err)
-		}
-		legalChar, err := ascii.CheckIllegalChar(inputParts) // Check for illegal characters in the string
-		if err != nil {
-			t.Error(err)
-		}
-		for _, part := range legalChar {
-			if part == "" {
-				fileString += "\n"
-				continue
-			}
-			for i := 0; i < 8; i++ {
-				for _, char := range part {
-					startingIndex := ascii.GetStartingIndex(int(char))
-					fileString += fileData[startingIndex+i]
-				}
-				fileString += "\n"
-			}
-
-		}
-		fileString += "\n"
-	}
-	expected.WriteString(fileString)
-
-	// Execute each command and store it in a string
-	for _, cmd := range commands {
-		// commandline output
-		output, err := cmd.Output()
-		if err != nil {
-			t.Error(err)
-		}
-		got.WriteString(string(output) + "\n")
-		//
-	}
-
-	// Read the output (1) of the commands from a file
-	exTest, err := os.ReadFile("got.txt")
-	if err != nil {
-		t.Error(err)
-	}
-	// Read the output (2) of the commands from a file
-	gotTest, err := os.ReadFile("expected.txt")
-	if err != nil {
-		t.Error(err)
-	}
-
-	// Match the strings of the file
-	if string(exTest) != string(gotTest) {
-		t.Errorf("Output does not match. Expected \n%v\n with length %v got \n%v\n with length %v", string(exTest), len(string(exTest)), (string(gotTest)), len(string(gotTest)))
 	}
 }
